e9e5f592af35fa49b629fd7e689c075e63ecd91d
fayiz770 <mohammadwaris456@gmail.com>
2023-06-27 14:38:11 -0700

    Just to patients

--- App.jsx
+++ App.jsx
@@ -23,6 +23,7 @@ export default function App() {
             setNotes(notesArray)
         })
         return unsubscribe
+        // This is an commit
     }, [])
     React.useEffect(() => {
         if(!currentNoteId){
